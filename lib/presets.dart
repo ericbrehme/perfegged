e34de35d321f68dc68829b769fa9924b6c2c5bfe
ericbrehme <59959547+ericbrehme@users.noreply.github.com>
2022-10-23 13:04:10 +0200

    laeuft noch nich ganz

--- lib/presets.dart
+++ lib/presets.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'functional_elements/appbar.dart';
 import 'dataclasses/preset.dart';
+import 'reusable_functions/jsonparser.dart';
 
 class Presets extends StatefulWidget {
   const Presets({ Key? key }) : super(key: key);
@@ -11,48 +10,56 @@ class Presets extends StatefulWidget {
   _PresetsState createState() => _PresetsState();
 }
 
-// Fetch content from the json file
+/* // Fetch content from the json file
   Future<List<Preset>> readPresets() async {
     //print("Test");
     final String response = await rootBundle.loadString('resources/presets.json');
     final List<dynamic> data =  jsonDecode(response).toList();
     final List<Preset> presets = data.map((i) => Preset(id: i[0],eggWeight: i[1], envTemp: i[2], yolkTemp: i[3])).toList();
     return presets;
-  }
+  } */
  
 class _PresetsState extends State<Presets> {
+ 
+  List presets = [];
 
-  List<Preset> _presetList = [];
+  @override
+  void initState() {
+    super.initState();
+    presets =  getDataFromJson('assets/data/presets.json');
+  }
 
-  
   @override
   Widget build(BuildContext context) {
       return Scaffold(
       appBar: MyAppBar(title: 'Presets'),
-      body: _buildList(),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+              presets.isNotEmpty
+                ? Expanded(
+                  child: ListView.builder(
+                    itemCount: presets.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        key: ValueKey(presets[index]['id']),
+                        margin: const EdgeInsets.all(10),
+                        child: ListTile(
+                          leading: Text(presets[index]['id'].toString()),
+                          title: Text(presets[index]['eggWeight'].toString()),
+                          subtitle: Text(presets[index]['yolkTemp'].toString()),
+                        ),
+                      );
+                    }
+                  ) 
+              )
+            : Container()
+          ]
+        )
+      ),
     );
   }
-
-  Widget _buildList() {
-    return FutureBuilder<List<Preset>>(
-      future: readPresets(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          List<ListTile> tiles = snapshot.data!
-            .map((c) => _tile(c.eggWeight.toString(), c.yolkTemp.toString(), c.id.toString())).toList();
-            return ListView(children: tiles);
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
-    );
-  }
-
-  ListTile _tile(String title, String subtitle, String id) => ListTile(
-    title: Text(title),
-    subtitle: Text(subtitle),
-    leading: Text(id),
-  ); 
 }
 
 
