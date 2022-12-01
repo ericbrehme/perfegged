4b4c1065619c533fbad4410418066066755d245e
ericbrehme <59959547+ericbrehme@users.noreply.github.com>
2022-12-01 14:58:12 +0100

    working timer

--- lib/homescreen_cook.dart
+++ lib/homescreen_cook.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:countdown_progress_indicator/countdown_progress_indicator.dart';
+import 'navigation.dart';
+import 'package:perfegged/functional_elements/appbar.dart';
 
 class HomescreenCook extends StatefulWidget {
-  const HomescreenCook({ Key? key }) : super(key: key);
+  const HomescreenCook({Key? key}) : super(key: key);
 
   @override
   _HomescreenCookState createState() => _HomescreenCookState();
 }
 
 class _HomescreenCookState extends State<HomescreenCook> {
+  final _controller = CountDownController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: MyAppBar(title: 'Perfegged'),
+      drawer: const Navigation(),
+      body: Center(
+        child: SizedBox(
+          height: 300,
+          width: 300,
+          child: CountDownProgressIndicator(
+            controller: _controller,
+            valueColor: Colors.red,
+            backgroundColor: Colors.blue,
+            initialPosition: 0,
+            duration: 20,
+            text: 'SEC',
+            onComplete: () => null,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/homescreen_setup.dart
+++ lib/homescreen_setup.dart
@@ -3,7 +3,7 @@ import 'navigation.dart';
 import 'package:perfegged/functional_elements/appbar.dart';
 
 class HomescreenSetup extends StatefulWidget {
-  const HomescreenSetup({ Key? key }) : super(key: key);
+  const HomescreenSetup({Key? key}) : super(key: key);
 
   @override
   _HomescreenSetupState createState() => _HomescreenSetupState();
@@ -13,16 +13,20 @@ class _HomescreenSetupState extends State<HomescreenSetup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(title: 'Perfegged'),
-      drawer: const Navigation(),
-      floatingActionButton: IconButton(
-        onPressed: () => Navigator.pushNamed(context, '/presets'),
-        icon: const Icon(Icons.content_paste_search),
-        tooltip: 'Presets',
-        iconSize: 50,
-      ),
-      body: Image(image: AssetImage('assets/images/clock.png')),
-    );
+        appBar: MyAppBar(title: 'Perfegged'),
+        drawer: const Navigation(),
+        floatingActionButton: IconButton(
+          onPressed: () => Navigator.pushNamed(context, '/presets'),
+          icon: const Icon(Icons.content_paste_search),
+          tooltip: 'Presets',
+          iconSize: 50,
+        ),
+        body: Center(
+            child: Column(children: <Widget>[
+          ElevatedButton(
+            child: const Text('Start Timer'),
+            onPressed: () => Navigator.pushNamed(context, '/homescreen_cook'),
+          )
+        ])));
   }
 }
-
